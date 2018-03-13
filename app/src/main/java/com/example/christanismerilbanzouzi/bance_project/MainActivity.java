0df052dcf97c1bb32fab8ac02a0613e457c16a6a
christanismerilbanzouzi <merilb78@gmail.com>
2018-03-13 19:14:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/christanismerilbanzouzi/bance_project/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.christanismerilbanzouzi.bance_project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+     private static int SPLASH_TIME_OUT=3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+                );
+        setContentView(R.layout.activity_main);
+         new Handler().postDelayed(new Runnable() {
+             @Override
+             public void run() {
+                 Intent homreIntent = new Intent(MainActivity.this,HomeActivity.class);
+                 startActivity(homreIntent);
+                 finish();
+             }
+         },SPLASH_TIME_OUT);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/christanismerilbanzouzi/bance_project/ShopActivity.java
@@ -0,0 +1,56 @@
+package com.example.christanismerilbanzouzi.bance_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ShopActivity extends AppCompatActivity {
+
+    private Toolbar myToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+        myToolBar =findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if( item.getItemId() == R.id.action_account){
+            Log.i("Action_Account", "In action Account ");
+            Intent sartItent = new Intent(getApplicationContext(),AccountActivity.class);
+            startActivity(sartItent);
+
+        }
+        else if (item.getItemId() == R.id.action_caddy){
+            Log.i("Action_Caddy", "In action Caddy ");
+            Intent sartItent = new Intent(getApplicationContext(),CaddyActivity.class);
+            startActivity(sartItent);
+        }
+        else if (item.getItemId() == R.id.action_shop){
+            Log.i("Action_Shop", "In action Shop");
+            Intent sartItent = new Intent(getApplicationContext(),ShopActivity.class);
+            startActivity(sartItent);
+        }
+        else{
+            Log.i("Action_Home", "In Other Options ");
+            Intent sartItent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(sartItent);
+
+        }
+        return true;
+    }
+}
