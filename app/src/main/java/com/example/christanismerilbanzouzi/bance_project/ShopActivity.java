124cab89239a15cb9587a48fbad8baf55cd0081b
christanismerilbanzouzi <merilb78@gmail.com>
2018-03-16 14:47:37 +0100

    Ajout de bouton sur la bar Toolbar

--- app/src/main/java/com/example/christanismerilbanzouzi/bance_project/ShopActivity.java
+++ app/src/main/java/com/example/christanismerilbanzouzi/bance_project/ShopActivity.java
@@ -58,6 +58,8 @@ public class ShopActivity extends AppCompatActivity {
             if(goToLocation.resolveActivity(getPackageManager())!=null){
                 startActivity(goToLocation);
             }
+
+
         }
         return true;
 
